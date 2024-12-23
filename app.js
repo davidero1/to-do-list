8e50fb7f60aa9c2d8459501c7a13f4e68f89229f
davidero01 <daviderophillips@gmail.com>
2024-12-23 11:37:38 +0100

    local

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ todoList.addEventListener("click", plusTrash); // find the function in line 66
 //  add event listeners for the filter
 filterOption.addEventListener("click", filterTodo);
 
+// add event listener to the entire documnt
+document.addEventListener("DOMContentLoaded", getTodos); // the function is written in line 202
+
 // Functions
 
 // FUNCTION 1 - ADD TO DO
@@ -34,6 +37,9 @@ function addTodo(e) {
   newTodo.innerText = todoInput.value; // so the typed text can show the as the true value
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
+
+  // Add to local storage
+  saveLocalTodos(todoInput.value); // argument invoked w function @ line 186
 
   // create button (check button)
   const completeButton = document.createElement("button"); // button
@@ -62,6 +68,9 @@ function plusTrash(e) {
   if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement; // get hold of the parent container
     todo.classList.toggle("fall"); // this adds the style of the class
+
+    removeLocalTodo(todo); // argument
+
     // todo.remove(); // this removes the element entirely and clears the space it held but loses the css effect
     todo.addEventListener("transitionend", () => {
       todo.remove();
@@ -110,3 +119,148 @@ function filterTodo(e) {
     }
   });
 }
+
+// local storage & session storage
+
+// they're basically written the same way
+
+// syntax: localStorage.setItem("key", "value"); // .setItem is used to set
+// whatever is passed into the bracket is a string
+// it can only accept strings
+// the key is written first, then the value
+
+// localStorage.setItem("todo", "write some code");
+
+// "write some code" shows in the Application tab of the browser terminal
+// local storage always persists until it is cleared
+
+// localStorage.setItem("todoList", "Take a nap");
+// session only lasts for that session, clears upon close
+
+// .clear is used to clear the storage
+// localStorage.clear("todo");
+
+// localStorage.setItem("user", "Peter");
+
+// localStorage.clear("user"); // clear is used to clear the entire data
+
+// localStorage.setItem("user", "Ade");
+// localStorage.setItem("user", "Swim");
+//  when the same key is used w different values, the most recent value will overide the earlier values
+
+// we can have multiple values with a single key using stringify
+
+// retrieving data from the local storage
+
+/*
+const todo = localStorage.getItem("todo");
+console.log(todo);
+*/
+
+// how to pass an array into a localStorage
+
+const todoItems = {
+  todo1: "write some code",
+  todo2: "play basketball",
+  todo3: "get milk",
+}; // change to [] and remove the todo1:-3
+
+localStorage.clear(); // to clear the storage
+
+localStorage.setItem("todo", todoItems);
+// when an array is passed into a local storage it changes it to a string
+
+// const retrieveItem = localStorage.getItem("todo");
+
+// console.log(retrieveItem); // even if it is retrived, it still shows as a string
+
+// the stringify and parse method is used to maintain the passed array
+
+// the stringify turns it to a string and the parse is sued to convert it to its original form as an array
+
+localStorage.setItem("todo", JSON.stringify(todoItems));
+// it appears as a string now
+
+// now lets retrieve w .parse
+const retrieveItem = JSON.parse(localStorage.getItem("todo"));
+// console.log(retrieveItem, typeof retrieveItem);
+// this returns it as an array ['object'] (check line :173 on the console tab)
+
+// Add todos to the local storage
+saveLocalTodos(todoInput.value);
+
+// function to save input into the local storage
+// this function is invoked @ line 42
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos)); // this saves the todo to the local storage
+}
+
+// retrieving the todos from local storage
+function getTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  // loop
+  todos.forEach((todo) => {
+    // a container for the todos
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    // create li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todo; // todo
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    // create button (check button)
+    const completeButton = document.createElement("button"); // button
+    completeButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = `<i class="fa-solid fa-rectangle-xmark"></i>`;
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // append the todoDiv to the todoList
+    todoList.appendChild(todoDiv);
+  });
+}
+
+// local storage functions
+// functionality to delete todo from the local storage entirely
+function removeLocalTodo(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = {};
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1); // w splice we can remove any element in any position from an array
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// const numbers = [1,2,3,4,5,6,7,8,9];
+// numbers.splice(1, 3); the first value is the starting point, while the 2nd is the number of elements to be removed
+// splice method alters the original array
+
+// const numbers = ["code", "swim", "eat", "nap"];
+// numbers.splice(0, 3); // result will show only nap
+// console.log(numbers);
+
+// write up on string methods, array methods: map, filter, reduce, sort, find, for each
